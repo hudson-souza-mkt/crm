b759de5988187c129e916455f890ffe33c24538c
[lasy] <git@lasy.ai>
2025-07-25 09:38:30 -0300

    [lasy] Corrigido erro de JSX no layout principal - escreveu 2 arquivo(s)

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -15,9 +15,9 @@ export function MainLayout() {
 
   return (
     <div className="flex min-h-screen w-full relative">
-      {/* Background com gradiente animado */}
+      {/* Background com gradiente animado e padrão SVG */}
       <div className="fixed inset-0 bg-gradient-to-br from-indigo-50 via-white to-purple-50 -z-10" />
-      <div className="fixed inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%239C92AC" fill-opacity="0.03"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] -z-10" />
+      <div className="fixed inset-0 bg-pattern -z-10" />
       
       <div 
         className={cn(
